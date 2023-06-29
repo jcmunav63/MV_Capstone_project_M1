f6933e2131857e3e57580c3773abc7871e8a4af7
jcmunav63 <jcmunav63@gmail.com>
2023-06-28 23:21:19 -0600

    Add elements and styles of mobile mennu to html, css & JS files.

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,1 +1,18 @@
 // FIle for Mobile Menu interaction
+const menuBtn = document.querySelector('.toggle-btn');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-btn');
+
+menuBtn.addEventListener('click', () => {
+  overlay.classList.add('show');
+});
+
+closeBtn.addEventListener('click', () => {
+  overlay.classList.remove('show');
+});
+
+overlay.querySelectorAll('a').forEach((link) => {
+  link.addEventListener('click', () => {
+    overlay.classList.remove('show');
+  });
+});
